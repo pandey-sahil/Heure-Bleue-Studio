d361cd32cf4bfc204f00b05a41686a1805115081
pandey-sahil <sahilp2593@gmail.com>
2023-10-13 14:03:44 +0530

    small changes

--- script.js
+++ script.js
@@ -33,14 +33,14 @@ loco();
 
 var tl = gsap.timeline();
 
-tl.to("#text h1:nth-child(1)",{
-    x:-190,
-},'a').to("#text h1:nth-child(3)",{
-    x:170,
-},'a').to("#text",{
-    backgroundColor:"#0149c60c",
-    duration:2
-},'b').to("#text h3",{
+tl.to("#text h1:nth-child(1)", {
+    x: -190,
+}, 'a').to("#text h1:nth-child(3)", {
+    x: 170,
+}, 'a').to("#text", {
+    backgroundColor: "#0149c60c",
+    duration: 2
+}, 'b').to("#text h3", {
     // display:"initial"
-    opacity:1
-},'b')
+    opacity: 1
+}, 'b')
