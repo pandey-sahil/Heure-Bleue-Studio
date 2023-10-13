278f87d89d76f4fa9caa4363848e80263bdd358a
pandey-sahil <sahilp2593@gmail.com>
2023-10-13 22:52:29 +0530

    created page2 with animations and created cursor

--- script.js
+++ script.js
@@ -31,16 +31,67 @@ function loco() {
 }
 loco();
 
-var tl = gsap.timeline();
+function loadingAnime() {
+    var crsr = document.querySelector('#cursor');
+    var main = document.querySelector("#main")
 
-tl.to("#text h1:nth-child(1)", {
-    x: -190,
-}, 'a').to("#text h1:nth-child(3)", {
-    x: 170,
-}, 'a').to("#text", {
-    backgroundColor: "#0149c60c",
-    duration: 2
-}, 'b').to("#text h3", {
-    // display:"initial"
-    opacity: 1
-}, 'b')
+    main.addEventListener("mousemove", function(dets){
+        // console.log(dets.x)
+        gsap.to(crsr,{
+            top: dets.y,
+            left: dets.x 
+        })
+    })
+
+
+    var tl = gsap.timeline();
+
+    tl.to("#text h1:nth-child(1)", {
+        x: -190,
+    }, 'a').to("#text h1:nth-child(3)", {
+        x: 170,
+    }, 'a').to("#text", {
+        backgroundColor: "#0149c60c",
+        duration: 2
+    }, 'b').to("#text h3", {
+        // display:"initial"
+        opacity: 1
+    }, 'b').to("#nav", {
+        opacity: 1,
+        delay: 1
+    }, 'b')
+}
+loadingAnime();
+
+function page2Anime(){
+
+    var tl = gsap.timeline({
+        scrollTrigger:{
+            start: "top 0%",
+            end:"top -80%",
+            scroller: "#main",
+            trigger:"#page2",
+            scrub: 1.5,
+            markers:true
+        }
+    })
+
+    tl.to("#line1",{
+        clipPath: 'inset(100% 0 0 0)'
+    },"ab")
+    .to("#nav h3, #nav svg",{
+        filter:"invert(0)",
+        color: 'black'
+    },'ab')
+    .to("#page2",{
+        backgroundColor:"#0148C6"
+    },"aa")
+    .to("#center svg",{
+        fill: '#000'
+    },"aa")
+    .from("#line2",{    
+        clipPath: 'inset(0 0 100% 0)'
+    })
+
+}
+page2Anime();
